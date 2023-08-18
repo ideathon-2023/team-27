425c1ae78ee4207b98aae34335a396e72eb96b6e
Ujjawalsidana <118710561+Ujjawalsidana@users.noreply.github.com>
2023-08-18 23:54:22 +0530

    Add files via upload

---
+++ connection2.php
@@ -0,0 +1,32 @@
+<?php
+
+$conn = mysqli_connect("localhost","root","","crud");
+
+if(isset($_POST['submit']))
+{
+    $name1= $_POST['Name'];
+    $att_sid=$_POST['SID'];
+    // echo $brands;
+
+    foreach($name1 as $item)
+    foreach($att_sid as $item2)
+
+    {
+        // echo $item . "<br>";
+        $query = "INSERT INTO record1(Name,attend_sid) VALUES ('$item','$item2')";
+        $query_run = mysqli_query($conn, $query);
+    }
+
+    if($query_run)
+    {
+     
+        header("Location: index.php");
+    }
+    else
+    {
+       
+        header("Location: index.php");     
+    }
+}
+?>
+
