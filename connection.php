e0c6cfec376ab1b1d6df820d1f413d8dff1d72e6
Ujjawalsidana <118710561+Ujjawalsidana@users.noreply.github.com>
2023-08-18 23:27:58 +0530

    connection.php

--- connection.php
+++ connection.php
@@ -3,9 +3,9 @@
     $username = "root";
     $password = "";
     $db_name = "crud";  
-    $conn = new mysqli($servername, $username, $password, $db_name, 3306);
+    $conn = new mysqli($servername, $username, $password, $db_name,3306);
     if($conn->connect_error){
-        die("Connection failed".$conn->connect_error);
+        die("Connection failed".$conn->connect_error);       
     }
     echo "";
     
