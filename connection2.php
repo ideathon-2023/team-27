c4cc66b0989365c3a04473c682c3e705a4a0a984
Ujjawalsidana <118710561+Ujjawalsidana@users.noreply.github.com>
2023-08-19 14:52:06 +0530

    connection2.php

--- connection2.php
+++ connection2.php
@@ -6,13 +6,12 @@ if(isset($_POST['submit']))
 {
     $name1= $_POST['Name'];
     $att_sid=$_POST['SID'];
-    // echo $brands;
 
     foreach($name1 as $item)
     foreach($att_sid as $item2)
 
     {
-        // echo $item . "<br>";
+        
         $query = "INSERT INTO record1(Name,attend_sid) VALUES ('$item','$item2')";
         $query_run = mysqli_query($conn, $query);
     }
